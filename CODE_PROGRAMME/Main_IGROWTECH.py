4101f7a888f3700ed371e6e9edd47f181c5f8299
Tungdil-main-dor <56158296+Tungdil-main-dor@users.noreply.github.com>
2020-04-01 16:35:18 +0200

    Update Main_IGROWTECH.py

--- CODE_PROGRAMME/Main_IGROWTECH.py
+++ CODE_PROGRAMME/Main_IGROWTECH.py
@@ -8,7 +8,7 @@ print("Configuration GPIO actuelle du Raspberry PI :", configuration)
 # Définis le mode d'adressage des pin GPIO comme étant BCM
 # PLus d'information sur https://pinout.xyz
 
-if configuration != "none":
+if configuration != "BCM":
     GPIO.setmode(GPIO.BCM)
 else :
     print(OK)
@@ -24,13 +24,22 @@ else :
 # Capteurs_lvl_BAC_Culture = 25
 # Capteurs_lvl_BAC_EAU = 9
 # Capteurs_lvl_BAC_engrais = 10
-# Pompe de brassage = alimenter en continue
-# Pompe a air = alimenter en continue
 
 
 # ici, les ports GPIO sont déclarer comme étant des sorties (OUT), initialiser à l'état HIGH ( activer )
 # Bien qu'il puisse (et même très probablement) par la suite être initialiser à LOW ( inactif )
-# les ligne de code GPIO.output permette de modifier les valeurs d'état initiales rapidement
+# les ligne de code GPIO.output permette de modifier les valeurs d'état 1 HIGH, 0 LOW
+
+Pompe_Arrosage = 21
+Pompe_Engrais = 26
+Pompe_Remplissage_BAC = 20
+Pompe_Vidange = 19
+LED = 16
+Ventilateur = 13
+Pompe_PH = 12
+# Pompe de brassage = alimenter en continue
+# Pompe a air = alimenter en continue
+# Caméra = alimenter par la carte raspberry pi
 
 GPIO.setup(Pompe_Arrosage,GPIO.OUT,initial=GPIO.HIGH)
 GPIO.output(Pompe_Arrosage,1)
@@ -53,19 +62,21 @@ try:
 
         request = input("Entre etat GPIO : \n 0 = desactiver \n 1 = activer \n On attend 6 valeurs a la suite, 0 ou 1, ecrit a la suite sans espace -->")
 
-        if (len(request) == 6):
+        if (len(request) == 7):
 
             GPIO.output(Pompe_Arrosage,int(request[0]))
 
             GPIO.output(Pompe_Engrais,int(request[1]))
 
-            GPIO.output(Pompe_Remplissage_reservoir_principale,int(request[2]))
+            GPIO.output(Pompe_Remplissage_BAC,int(request[2]))
 
             GPIO.output(Pompe_Vidange,int(request[3]))
 
             GPIO.output(LED,int(request[4]))
 
             GPIO.output(Ventilateur,int(request[5]))
+
+            GPIO.output(Pompe_PH,int(request[6]))
         else :
             print("pas de violence, lire : append.... !!! \n cordialement <3 \n \n")
 
